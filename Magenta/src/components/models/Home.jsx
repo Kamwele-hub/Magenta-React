619e27cbcd120467db0c31569390fd2451262340
Stephen Kamwele <stephen.kamwele@student.moringaschool.com>
2023-06-08 09:54:14 +0300

    modified login.jsx

--- Magenta/src/components/models/Home.jsx
+++ Magenta/src/components/models/Home.jsx
@@ -3,28 +3,34 @@ import Register from "../models/Register";
 import Login from "../models/Login";
 
 function Home() {
-  const [displayRegister,displayLogin] = useState(true)
-  const changeForm = () =>{
-    displayLogin(!displayRegister)
+  const [displayRegister, setDisplayRegister] = useState(true);
+
+  const changeForm = () => {
+    setDisplayRegister(!displayRegister);
+  };
+
+  if (displayRegister === true) {
+    return (
+      <>
+        <h1>Petfinder</h1>
+        <Register changeForm={changeForm} />
+        <span onClick={changeForm} className="lUser">
+          Already registered? 
+          <div className="hover">
+            Go to login
+          </div>
+        </span>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h1>Petfinder</h1>
+        <Login changeForm={changeForm} />
+        <p onClick={changeForm} className="lUser">Go back to registration</p>
+      </>
+    );
   }
-if(displayRegister === true){
-  return (<>
-    <h1>Petfinder</h1>
-    <Register changeForm={changeForm}/>
-    <span onClick={changeForm} className="lUser">Already registered? 
-    <div className="hover">
-    Go to login
-    </div>
-    </span>
-    
-    </>);
-}else{
-  return (<>
-    <h1 >Petfinder</h1>
-    <Login changeForm={changeForm}/>
-    <p  onClick={changeForm}  className="lUser">Go back to registration</p>
-    </>);
-}
 }
 
 export default Home;
--- Magenta/src/components/models/Login.jsx
+++ Magenta/src/components/models/Login.jsx
@@ -1,73 +1,77 @@
 import { useNavigate } from "react-router-dom";
-import { useState ,useEffect } from "react";
-
+import { useState, useEffect } from "react";
 import axios from "axios";
 import { useStore } from "zustand";
-import {  petsStore } from "../store/PetsKeeper";
+import { petsStore } from "../store/PetsKeeper";
 
 function Login() {
-  const pets = useStore(petsStore)
-  const [users,setUsers] = useState([])
-  console.log(users)
-  useEffect(() =>{
-   axios.get("https://magenta.onrender.com").then((r) =>
-    setUsers(r.data),
-    console.log(users)
-   )
-  }, [])
+  const pets = useStore(petsStore);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://magenta.onrender.com").then((r) => {
+      setUsers(r.data);
+      console.log(r.data);
+    });
+  }, []);
+
   const [login, setLogin] = useState({
     name: "",
     password: "",
   });
-  
+
   const redirect = useNavigate();
+
   const handleSubmit = (e) => {
-     e.preventDefault();
-    //map through the users array
-    let exixting_user = users.filter((user) =>{
-      return user.name === login.name
-    })
-    if(exixting_user.length !== 0){
-    if(exixting_user[0].password === login.password){
-    
-      axios.get(`https://magentaa.onrender.com/pets/${exixting_user[0].name}`).then((r) =>{
-      if(r.data.length === 0){
-        pets.setPetsStore([{
-        id:null,
-        name:"",
-        breed:"",
-        image:"",
-        user_id:exixting_user[0].id
-            }]) 
-            redirect("/mypets")}else{
-        console.log(r.data)
-        pets.setPetsStore(r.data) 
-        redirect("/mypets")} 
-      }    
-      )
- 
-    }else{
-      alert("Incorrect password")
-    }}else{
-      alert("Incorrect name")
+    e.preventDefault();
+
+    let existing_user = users.find((user) => user.name === login.name);
+
+    if (existing_user) {
+      if (existing_user.password === login.password) {
+        axios.get(`https://magenta.onrender.com/pets/${existing_user.name}`).then((r) => {
+          if (r.data.length === 0) {
+            pets.setPetsStore([
+              {
+                id: null,
+                name: "",
+                breed: "",
+                image: "",
+                user_id: existing_user.id,
+              },
+            ]);
+            redirect("/mypets");
+          } else {
+            console.log(r.data);
+            pets.setPetsStore(r.data);
+            redirect("/mypets");
+          }
+        });
+      } else {
+        alert("Incorrect password");
+      }
+    } else {
+      alert("Incorrect name");
     }
-  
-   
   };
+
   return (
-    <div >
-    <form onSubmit={handleSubmit} className="signin">
-      <input
-        type="text"
-        placeholder="name"
-        onChange={(e) => setLogin({ ...login, name: e.target.value })}
-      ></input>
-      <input type="password" placeholder="password" onChange={(e) => setLogin({
-        ...login, password:e.target.value
-      })} ></input>
-      <button >Login</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit} className="signin">
+        <input
+          type="text"
+          placeholder="name"
+          onChange={(e) => setLogin({ ...login, name: e.target.value })}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          onChange={(e) => setLogin({ ...login, password: e.target.value })}
+        />
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
+
 export default Login;
