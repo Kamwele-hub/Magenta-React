4a5a81f5b35cd969d8c951aadf63bd397811c67b
Stephen Kamwele <stephen.kamwele@student.moringaschool.com>
2023-06-08 11:37:10 +0300

    app.js!

--- Magenta/magenta-app/src/App.js
+++ Magenta/magenta-app/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './CSS/App.css';
+import Home from './models/Home';
+import AllPets from './models/Allpets';
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import Mypets from './models/Mypets';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Routes>
+    <Route exact path="/" element={<Home/>}/>
+    <Route exact path="/mypets" element={<Mypets/>}/>
+    <Route exact path="/allpets" element={<AllPets/>}/>
+    </Routes>
+    </Router>
   );
 }
 
