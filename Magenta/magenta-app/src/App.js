82dd307dba24eaf480a1592228b0d52c9a3f804d
Stephen Kamwele <stephen.kamwele@student.moringaschool.com>
2023-06-09 00:50:41 +0300

    fixed bugs

--- Magenta/magenta-app/src/App.js
+++ Magenta/magenta-app/src/App.js
@@ -1,18 +1,21 @@
-import './CSS/App.css';
-import Home from './models/Home';
-import AllPets from './models/AllPets';
-import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
-import Mypets from './models/MyPets';
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./models/Home";
+import AllPets from "./models/AllPets";
+import Mypets from "./models/MyPets";
+import Register from "./models/Register.js";
+import CSS from "./CSS/App.css"
+import NavBar from "./components/NavBar.js"
 
 function App() {
   return (
     <Router>
-    <Routes>
-    <Route exact path="/" element={<Home/>}/>
-    <Route exact path="/mypets" element={<Mypets/>}/>
-    <Route exact path="/allpets" element={<AllPets/>}/>
-    </Routes>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/mypets" element={<Mypets />} />
+        <Route exact path="/allpets" element={<AllPets />} />
+        <Route exact path="/register" element={<Register changeForm={() => {}} />} />
+      </Routes>
     </Router>
   );
 }
